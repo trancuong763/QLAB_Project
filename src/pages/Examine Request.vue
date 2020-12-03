170a8ab1bd83f125e8d060e916af7b7840e9643d
XuanSon99 <xuansonn12499@gmail.com>
2020-12-03 09:43:25 +0700

    change

--- src/pages/Examine Request.vue
+++ src/pages/Examine Request.vue
@@ -54,6 +54,9 @@
           ></span>
           <span v-if="isSearching">Đang tìm...</span>
         </div>
+        <!-- <div class="loading">
+          <div class="lds-ring"><div></div><div></div><div></div></div>
+        </div> -->
       </div>
     </div>
     <div class="row" v-if="errors != []">
@@ -149,9 +152,7 @@ export default {
     loader() {
       const l = this.loader;
       this[l] = !this[l];
-
       setTimeout(() => (this[l] = false), 3000);
-
       this.loader = null;
     },
   },
@@ -274,6 +275,52 @@ export default {
     transform: rotate(0);
   }
   to {
+    transform: rotate(360deg);
+  }
+}
+.loading{
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.1);
+  z-index: 999;
+}
+.lds-ring {
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+  top: 50%;
+  left: 50%;
+}
+.lds-ring div {
+  box-sizing: border-box;
+  display: block;
+  position: absolute;
+  width: 64px;
+  height: 64px;
+  margin: 8px;
+  border: 8px solid #fff;
+  border-radius: 50%;
+  animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+  border-color: #fff transparent transparent transparent;
+}
+.lds-ring div:nth-child(1) {
+  animation-delay: -0.45s;
+}
+.lds-ring div:nth-child(2) {
+  animation-delay: -0.3s;
+}
+.lds-ring div:nth-child(3) {
+  animation-delay: -0.15s;
+}
+@keyframes lds-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
     transform: rotate(360deg);
   }
 }
